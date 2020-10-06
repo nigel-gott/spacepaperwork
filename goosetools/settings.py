37f101c6b70bf8be7ab08e27bca1a43461359d73
Nigel Gott <nigel.gott@gmail.com>
2020-10-06 14:52:59 +0100

    Pull allowed_hosts out to env file so can be configured for localhost in dev and the actual domain in prod

--- goosetools/settings.py
+++ goosetools/settings.py
@@ -43,7 +43,7 @@ DATABASES = {
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['goosetools.com', 'www.goosetools.com']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Application definition
 
