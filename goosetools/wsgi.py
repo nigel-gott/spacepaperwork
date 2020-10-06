dd101adb70a79f61c96ff5316cd7de5081a7154f
Nigel Gott <nigel.gott@gmail.com>
2020-10-06 15:04:47 +0100

    Add run script

--- goosetools/wsgi.py
+++ goosetools/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goldengoose.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goosetools.settings')
 
 application = get_wsgi_application()
