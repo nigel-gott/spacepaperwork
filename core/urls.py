5a53e24a5ce4a982e990f7e647e300eb0380c78d
Nigel Gott <nigel.gott@gmail.com>
2020-10-14 13:08:10 +0100

    Switch to card layout

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('settings/', SettingsView.as_view(), name='settings'),
     path('fleet/create/', fleet_create, name='fleet_create'),
     path('fleet/<int:pk>/', fleet_view, name='fleet_view'),
+    path('fleet/end/<int:pk>/', fleet_view, name='fleet_end'),
+    path('fleet/edit/<int:pk>/', fleet_view, name='fleet_edit'),
 ]
