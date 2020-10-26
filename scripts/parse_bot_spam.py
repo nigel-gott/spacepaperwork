4149f2b335acd0f26dfbeb16150c0c30dbd8d7c4
Nigel Gott <nigel.gott@gmail.com>
2020-10-26 14:05:39 +0000

    add new way of parsing char data and updated fixture

--- scripts/parse_bot_spam.py
+++ scripts/parse_bot_spam.py
@@ -4,47 +4,97 @@ import glob
 import os
 import csv
 
-def diff(auth_data, from_chat, auth_usernames):
-    in_auth_not_chat = set(auth_data.keys())-set(from_chat.keys())
-    in_chat_not_auth = set(from_chat.keys())-set(auth_data.keys())
-    names = set()
-    for uid in in_auth_not_chat:
-        names.add(auth_usernames[uid])
-    print("Discord IDs in auth data but not in chat: " + str(names))
-    print(in_chat_not_auth)
-    for uid, data in auth_data.items():
-        if uid in from_chat:
-            for char, corp in data.items():
-                if char not in from_chat[uid]:
-                    print("Auth data has " + uid + " - " + char)
 
-def main():
-    chat_filenames = glob.glob('discord_channel_dump/*.json')
-    if len(chat_filenames) != 1:
-        raise Exception("Found multiple or no chat files in sub dir: " + str(chat_filenames))
-    chat_filename = chat_filenames[0]
+class Character:
+    def __init__(self, pk, ingame_name, corp, discord_user_pk):
+        self.pk = pk
+        self.ingame_name = ingame_name
+        self.corp = corp.upper()
+        self.discord_user_pk = discord_user_pk
 
+    def __str__(self):
+        return f"pk({self.pk}), ingame_name({self.ingame_name}), corp({self.corp}), discord_user_pk({self.discord_user_pk})" 
+
+    def __eq__(self, obj):
+        return isinstance(obj, Character) and obj.pk == self.pk and obj.ingame_name == self.ingame_name and obj.corp == self.corp and self.discord_user_pk == obj.discord_user_pk
+
+class DiscordUser:
+    def __init__(self, pk, uid, avatar_hash, username):
+        self.pk = pk
+        self.uid = uid
+        self.avatar_hash = avatar_hash
+        self.username = username
+        self.characters = {}
     
-    auth_data = {}
-    auth_usernames = {}
+    def add_character(self, character):
+        if character.ingame_name in self.characters:
+            raise Exception("Dup char " + character.ingame_name)
+        else:
+            self.characters[character.ingame_name] = character
+    
+    def __str__(self):
+        return f"pk({self.pk}), uid({self.uid}), hash({self.avatar_hash}), username({self.username}), chars({self.characters})"
+
+    def __eq__(self, obj):
+        return isinstance(obj, DiscordUser) and obj.pk == self.pk and obj.uid == self.uid and obj.avatar_hash == self.avatar_hash and self.username == obj.username
+
+
+
+def load_existing_models():
+    with open(os.path.join(os.curdir, "../misc_data/char_dump.json"), "r") as existing_models_file:
+        existing_models = json.load(existing_models_file)
+        existing_characters = {}
+        existing_users = {}
+        users_by_pk = {}
+        next_character_pk = 1
+        next_discord_user_pk = 0
+        for model in existing_models:
+            if model['model'] == 'core.discorduser':
+                fields = model['fields']
+                uid = fields['uid']
+                if uid in existing_characters:
+                    raise Exception("Duplicate " + uid)
+                else:
+                    existing_users[uid] = DiscordUser(model['pk'], fields['uid'], fields['avatar_hash'], fields['username'])
+                    users_by_pk[model['pk']] = existing_users[uid]
+                next_discord_user_pk = max(model['pk']+1, next_discord_user_pk)
+        for model in existing_models:
+            if model['model'] == 'core.character':
+                fields = model['fields']
+                ingame_name = fields['ingame_name']
+                if ingame_name in existing_characters:
+                    raise Exception("Duplicate " + ingame_name)
+                else:
+                    char = Character(model['pk'], fields['ingame_name'], fields['corp'], fields['discord_user']) 
+                    existing_characters[ingame_name] = char
+                    users_by_pk[fields['discord_user']].add_character(char)
+                next_character_pk = max(model['pk']+1, next_character_pk)
+        return existing_characters, existing_users, next_character_pk, next_discord_user_pk
+
+def load_kris_dump(chars, users, char_pk, user_pk):
     with open(os.path.join(os.curdir, "../misc_data/auth.csv"), "r", newline="") as auth_csv:
         lines = list(csv.reader(auth_csv))[1:]
         for line in lines:
             uid = line[0]
             discord_name = line[1]
-            user_name = line[2]
+            ingame_name = line[2]
             corp = line[3]
-            if uid not in auth_data:
-                auth_data[uid] = {}
-            auth_data[uid][user_name] = corp 
-            if uid not in auth_usernames:
-                auth_usernames[uid] = discord_name
-            else:
-                if auth_usernames[uid] != discord_name:
-                    raise Exception(f"Found mis matching username for {uid}, {discord_name} vs {auth_usernames[uid]}")
-    with open("../misc_data/discord_id_to_char_auth.json", "w") as out_file:
-        out_file.write(jsonpickle.encode(auth_data, indent=4))
+            if uid not in users:
+                users[uid] = DiscordUser(user_pk, uid, None, discord_name) 
+                user_pk = user_pk + 1
+            
+            if ingame_name not in chars:
+                chars[ingame_name] = Character(char_pk, ingame_name, corp, users[uid].pk)
+                users[uid].add_character(chars[ingame_name])
+                char_pk = char_pk + 1
     
+    return chars, users, char_pk, user_pk 
+
+def parse_chat_data():
+    chat_filenames = glob.glob('../misc_data/Gooseflock Featheration - stupid Goon chat - general-bot-spam*.json')
+    if len(chat_filenames) != 1:
+        raise Exception("Found multiple or no chat files in sub dir: " + str(chat_filenames))
+    chat_filename = chat_filenames[0]
     with open(os.path.join(os.curdir, chat_filename), "r") as json_chat_file:
         chat = json.load(json_chat_file)
         discord_id_to_characters = {}
@@ -65,53 +115,87 @@ def main():
                 if content.lower().startswith("!addign"):
                     if author_id not in discord_id_to_characters:
                         discord_id_to_characters[author_id] = {}
-                    # print(f"Adding {character} to {author_id}")
                     discord_id_to_characters[author_id][character] = user_name
                 if content.lower().startswith("!removeign"):
                     if author_id not in discord_id_to_characters:
                         discord_id_to_characters[author_id] = {}
-                    # print(f"Removing {character} to {author_id}")
                     if character not in discord_id_to_characters[author_id]:
                         unknown_removes.append(f"{author_id}, {message['id']}, {character}")
                     else:
                         discord_id_to_characters[author_id].pop(character, None)
         with open("../misc_data/discord_id_to_char.json", "w") as out_file:
             out_file.write(jsonpickle.encode(discord_id_to_characters, indent=4))
+    return discord_id_to_characters, author_info
 
-        diff(auth_data, discord_id_to_characters, auth_usernames)
+def load_chat_data(chars, users, char_pk, user_pk):
+    discord_id_to_characters, author_info = parse_chat_data()
+    for uid, chat_data in discord_id_to_characters.items():
+        if uid not in users:
+            new_user = DiscordUser(user_pk, uid, url_to_hash(author_info[uid]['avatar_url']), author_info[uid]['discord_name']) 
+            users[uid] = new_user
+            user_pk = user_pk + 1
+        elif users[uid].avatar_hash is None:
+            new_user = DiscordUser(users[uid].pk, uid, url_to_hash(author_info[uid]['avatar_url']), author_info[uid]['discord_name']) 
+            new_user.characters = users[uid].characters
+            users[uid] = new_user 
+        else:
+            new_user = DiscordUser(users[uid].pk, uid, url_to_hash(author_info[uid]['avatar_url']), author_info[uid]['discord_name']) 
+            new_user.characters = users[uid].characters
+            if new_user != users[uid]:
+                print(f"Diff!\n{new_user}\n{users[uid]}")
+                users[uid] = new_user
 
-        models = []
-        next_character_pk = 1
-        next_discord_user_pk = 0
-        with open("../misc_data/characters.json", "w") as out_file:
-            for discord_id, character_to_corp in discord_id_to_characters.items():
-                avatar_url = author_info[discord_id]['avatar_url']
-                avatar_hash = avatar_url.split('/')[-1].split('.')[0]
-                models.append({
-                    "model": "core.discorduser",
-                    "pk": next_discord_user_pk,
-                    "fields": {
-                        "uid": discord_id,
-                        "username": author_info[discord_id]['discord_name'],
-                        "avatar_hash": avatar_hash,
-                        "shortened_uid": False,
-                        "unknown": False
-                    }
-                })
+        for ingame_name, corp in chat_data.items():
+            if ingame_name not in chars:
+                new_character = Character(char_pk, ingame_name, corp, users[uid].pk)
+                chars[ingame_name] = new_character
+                users[uid].add_character(new_character)
+                char_pk = char_pk + 1
+            else:
+                new_character = Character(chars[ingame_name].pk, ingame_name, corp, users[uid].pk)
+                if new_character != chars[ingame_name]:
+                    print(f"Diff!\n{new_character}\n{chars[ingame_name]}")
+    return chars, users, char_pk, user_pk
 
-                for character, user_name in character_to_corp.items():
-                    models.append({
-                        "model": "core.character",
-                        "pk": next_character_pk,
-                        "fields": {
-                            "discord_user": next_discord_user_pk,
-                            "ingame_name": character,
-                            "corp": user_name.upper()
-                        }
-                    })
-                    next_character_pk = next_character_pk + 1
-                next_discord_user_pk = next_discord_user_pk + 1
-            out_file.write(jsonpickle.encode(models, indent=4))
+
+def url_to_hash(url):
+    return url.split('/')[-1].split('.')[0]
+
+
+def output_models(chars, users):
+    models = []
+    with open("../misc_data/characters_and_users.json", "w") as out_file:
+        for char in chars.values():
+            models.append({
+                "model": "core.character",
+                "pk": char.pk,
+                "fields": {
+                    "discord_user": char.discord_user_pk,
+                    "ingame_name": char.ingame_name,
+                    "corp": char.corp.upper()
+                }
+            })
+        for user in users.values():
+            models.append({
+                "model": "core.discorduser",
+                "pk": user.pk,
+                "fields": {
+                    "uid": user.uid,
+                    "username": user.username, 
+                    "avatar_hash": user.avatar_hash,
+                    "unknown": False
+                }
+            })
+
+        out_file.write(jsonpickle.encode(models, indent=4))
+
+
+
+def main():
+    chars, users, next_char_pk, next_user_pk = load_existing_models()
+    chars, users, next_char_pk, next_user_pk = load_kris_dump(chars, users, next_char_pk, next_user_pk)
+    chars, users, next_char_pk, next_user_pk = load_chat_data(chars, users, next_char_pk, next_user_pk)
+    output_models(chars, users)
 
 
 def valid_command(content):
