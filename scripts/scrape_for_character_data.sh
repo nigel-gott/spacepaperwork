9221b6da94bfaa9f4b93011b8519e5285bb81055
Nigel Gott <nigel.gott@gmail.com>
2020-11-02 09:44:11 +0000

    Add some nice echoes

--- scripts/scrape_for_character_data.sh
+++ scripts/scrape_for_character_data.sh
@@ -6,6 +6,9 @@ token=`cat ~/.secrets/.secret_discord_token`
 
 source /opt/goosetools_venv/bin/activate
 
+echo "======================================"
+echo "Char scrape run begin!"
+date
 rm -rf misc_data/discord_channel_dump/
 mkdir -p misc_data/discord_channel_dump 
 docker run --rm -v $(pwd)/misc_data/discord_channel_dump:/app/out tyrrrz/discordchatexporter:stable export -c 754139786679156807 -t $token -f Json
@@ -13,5 +16,6 @@ python3 manage.py dbbackup
 python3 manage.py dumpdata core.Character core.DiscordUser > misc_data/char_dump.json
 python3 scripts/parse_bot_spam.py 
 python3 manage.py loaddata characters_and_users 
+echo "Char scrape end!"
 
 
