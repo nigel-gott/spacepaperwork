d365a943ebce65fb413606c731a93769e4df7bab
Nigel Gott <nigel.gott@gmail.com>
2020-11-02 09:30:21 +0000

    add source to scrape script

--- scripts/scrape_for_character_data.sh
+++ scripts/scrape_for_character_data.sh
@@ -4,12 +4,14 @@ IFS=$'\n\t'
 
 token=`cat ~/.secrets/.secret_discord_token`
 
+source /opt/goosetools_venv/bin/activate
+
 rm -rf misc_data/discord_channel_dump/
 mkdir -p misc_data/discord_channel_dump 
 docker run --rm -it -v $(pwd)/misc_data/discord_channel_dump:/app/out tyrrrz/discordchatexporter:stable export -c 754139786679156807 -t $token -f Json
-python manage.py dbbackup
-python manage.py dumpdata core.Character core.DiscordUser > misc_data/char_dump.json
-python scripts/parse_bot_spam.py 
-python manage.py loaddata core.characters_and_users.json
+python3 manage.py dbbackup
+python3 manage.py dumpdata core.Character core.DiscordUser > misc_data/char_dump.json
+python3 scripts/parse_bot_spam.py 
+python3 manage.py loaddata core.characters_and_users.json
 
 
