e646aff7bde4293971e8511dcb9363b963aa1af2
Nigel Gott <nigel.gott@gmail.com>
2020-11-05 20:44:42 +0000

    Remove extra password hashers currently not installed and breaking signup

--- config/settings/base.py
+++ config/settings/base.py
@@ -140,17 +140,6 @@ LOGIN_REDIRECT_URL = "fleet"
 # LOGIN_REDIRECT_URL = 'fleet'
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
 LOGIN_URL = "account_login"
-
-# PASSWORDS
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
-PASSWORD_HASHERS = [
-    # https://docs.djangoproject.com/en/dev/topics/auth/passwords/#using-argon2-with-django
-    "django.contrib.auth.hashers.Argon2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
-    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
-]
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
     {
