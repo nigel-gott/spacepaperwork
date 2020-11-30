ca19e05f4b16fd2b03784d3cf9a759529ee5e11b
Nigel Gott <nigel.gott@gmail.com>
2020-11-30 10:47:33 +0000

    Add admin site link for staff members and register ship in admin site.

--- goosetools/industry/admin.py
+++ goosetools/industry/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
-from goosetools.industry.models import ShipOrder
+from goosetools.industry.models import Ship, ShipOrder
+
+
+class ShipAdmin(admin.ModelAdmin):
+    search_fields = ("name", "tech_level", "free")
+
 
 admin.site.register(ShipOrder)
+admin.site.register(Ship, ShipAdmin)
