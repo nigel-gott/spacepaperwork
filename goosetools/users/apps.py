356866c8026f27d2537bc46a3f9a1dfc7a3c0aab
Nigel Gott <nigel.gott@gmail.com>
2020-12-01 15:23:43 +0000

    Dont accidentally comment out the code that does the feature...

--- goosetools/users/apps.py
+++ goosetools/users/apps.py
@@ -23,23 +23,20 @@ class UsersConfig(AppConfig):
     verbose_name = "Users"
 
     def ready(self):
-        pass
-        # from allauth.socialaccount.providers.discord.views import DiscordOAuth2Adapter
+        from allauth.socialaccount.providers.discord.views import DiscordOAuth2Adapter
 
-        # def complete_login_with_guild_roles(self, request, _app, token, **_kwargs):
-        #     headers = {
-        #         "Authorization": "Bearer {0}".format(token.token),
-        #         "Content-Type": "application/json",
-        #     }
-        #     extra_data = requests.get(self.profile_url, headers=headers).json()
-        #     if "id" in extra_data:
-        #         extra_data = {
-        #             **extra_data,
-        #             **_try_lookup_guild_roles(extra_data["id"]),
-        #         }
+        def complete_login_with_guild_roles(self, request, _app, token, **_kwargs):
+            headers = {
+                "Authorization": "Bearer {0}".format(token.token),
+                "Content-Type": "application/json",
+            }
+            extra_data = requests.get(self.profile_url, headers=headers).json()
+            if "id" in extra_data:
+                extra_data = {
+                    **extra_data,
+                    **_try_lookup_guild_roles(extra_data["id"]),
+                }
 
-        #     return self.get_provider().sociallogin_from_response(
-        #         request, extra_data_with_roles
-        #     )
+            return self.get_provider().sociallogin_from_response(request, extra_data)
 
-        # DiscordOAuth2Adapter.complete_login = complete_login_with_guild_roles
+        DiscordOAuth2Adapter.complete_login = complete_login_with_guild_roles
