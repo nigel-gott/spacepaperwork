a3c544bb48c7ee604391cefb1ca8dc9e61a87108
Nigel Gott <nigel.gott@gmail.com>
2020-12-01 17:26:32 +0000

    Add str for ship orders and better admin view.

--- goosetools/industry/admin.py
+++ goosetools/industry/admin.py
@@ -23,7 +23,34 @@ class ShipAdmin(admin.ModelAdmin):
     search_fields = ("name", "tech_level", "free")
     list_display = ["name", "tech_level", "free"]
     actions = [make_paid, make_free]
+    # recipient_character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    # assignee = models.ForeignKey(
+    #     settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True
+    # )
+    # contract_made = models.BooleanField(default=False)
+    # ship = models.ForeignKey(Ship, on_delete=models.CASCADE)
+    # uid = models.TextField(unique=True)
+    # quantity = models.PositiveIntegerField()
+    # created_at = models.DateTimeField()
+    # state = FSMField(default="not_started")
+    # notes = models.TextField(blank=True)
+    # payment_method = models.TextField(choices=PAYMENT_METHODS)
 
 
-admin.site.register(ShipOrder)
+class ShipOrderAdmin(admin.ModelAdmin):
+    search_fields = ("uid", "ship", "state", "assignee", "recipient_character")
+    list_display = [
+        "uid",
+        "ship",
+        "quantity",
+        "state",
+        "created_at",
+        "notes",
+        "assignee",
+        "recipient_character",
+        "payment_method",
+    ]
+
+
+admin.site.register(ShipOrder, ShipOrderAdmin)
 admin.site.register(Ship, ShipAdmin)
