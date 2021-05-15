d09fe8659142c0c368869518e6ace03c65f44ed5
Nigel Gott <nigel.gott@gmail.com>
2021-05-15 11:27:54 +0100

    Move import to post_migrate

--- goosetools/global_items/apps.py
+++ goosetools/global_items/apps.py
@@ -1,4 +1,14 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+# pylint: disable=unused-argument
+def _run_global_items_import(sender, **kwargs):
+    from goosetools.global_items.management.commands.item_data import (
+        import_global_items_from_global_files,
+    )
+
+    import_global_items_from_global_files()
 
 
 class GlobalItemsConfig(AppConfig):
@@ -6,8 +16,4 @@ class GlobalItemsConfig(AppConfig):
     verbose_name = "Global Items"
 
     def ready(self) -> None:
-        from goosetools.global_items.management.commands.item_data import (
-            import_global_items_from_global_files,
-        )
-
-        import_global_items_from_global_files()
+        post_migrate.connect(_run_global_items_import, sender=self)
