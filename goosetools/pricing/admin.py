465193a59e9958925c403f4e122a45573fd0eaf9
Nigel Gott <nigel.gott@gmail.com>
2021-06-14 19:31:24 +0100

    Add price list to admin site.

--- goosetools/pricing/admin.py
+++ goosetools/pricing/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from goosetools.pricing.models import ItemMarketDataEvent
+from goosetools.pricing.models import ItemMarketDataEvent, PriceList
 
 admin.site.register(ItemMarketDataEvent)
+admin.site.register(PriceList)
